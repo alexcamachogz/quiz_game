9f5df9330d3b25ecb0481ba62c198eaa785e5d81
Alex Camacho <camacho.ale94@gmail.com>
2023-07-04 12:15:46 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,106 @@
+question_data = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Super Mario 64, collecting 100 coins on a level will give you a 1-UP.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The programming language Python is based off a modified "
+                    "version of JavaScript.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States Department of Homeland Security was formed "
+                    "in response to the September 11th attacks.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Cartoon & Animations",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Bill Cipher in the show Gravity Falls is the good guy.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Board Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The card game Munchkin won the 2001 Origins Award for Best Traditional Card Game.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The alternative rock band, They Might Be Giants, released their album &#039;Flood&#039; in 1990. ",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The name of the attack Kamehameha in Dragon Ball Z was named "
+                    "after a famous king of Hawaii.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Romanian belongs to the Romance language family, shared with French, Spanish, "
+                    "Portuguese and Italian. ",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The freshwater amphibian, the Axolotl, can regrow it&#039;s limbs.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    }
+]
